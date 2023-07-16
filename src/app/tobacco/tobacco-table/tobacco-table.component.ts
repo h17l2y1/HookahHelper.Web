aab151ed7b3070b36fadfb4b7794919b74617fcd
vlad <vladislavvillya@gmail.com>
2023-07-16 18:28:39 +0300

    fix tobacco loading

--- src/app/tobacco/tobacco-table/tobacco-table.component.ts
+++ src/app/tobacco/tobacco-table/tobacco-table.component.ts
@@ -9,43 +9,16 @@ import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
 import {Tobacco} from "../../interfaces/entity/tobacco";
 
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-];
-
 @Component({
   selector: 'app-tobacco-table',
   templateUrl: './tobacco-table.component.html',
   styleUrls: ['./tobacco-table.component.scss']
 })
 export class TobaccoTableComponent implements AfterViewInit  {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  public readonly displayedColumns: string[] = ['image', 'name', 'description', 'country'];
 
   public dataSource!: MatTableDataSource<Tobacco>;
-
-  // dataSource = ELEMENT_DATA;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -84,7 +57,6 @@ export class TobaccoTableComponent implements AfterViewInit  {
       const xxx = result;
     });
   }
-
 
   private getTobaccos(): void {
     merge(this.sort.sortChange, this.paginator.page)
--- src/app/tobacco/tobacco.module.ts
+++ src/app/tobacco/tobacco.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TobaccoCreateComponent } from './tobacco-create/tobacco-create.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TobaccoCreateComponent} from './tobacco-create/tobacco-create.component';
 import {TobaccoRoutingModule} from "./tobacco-routing.module";
-import { TobaccoTableComponent } from './tobacco-table/tobacco-table.component';
+import {TobaccoTableComponent} from './tobacco-table/tobacco-table.component';
 import {MatTableModule} from "@angular/material/table";
 import {MatButtonModule} from "@angular/material/button";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatInputModule} from "@angular/material/input";
 import {TobaccoService} from "./tobacco.service";
 import {SharedModule} from "../shared/shared.module";
-import {BrandRoutingModule} from "../brand/brand-routing.module";
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatSelectModule} from "@angular/material/select";
 import {MatIconModule} from "@angular/material/icon";
@@ -41,4 +40,5 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
     TobaccoService
   ]
 })
-export class TobaccoModule { }
+export class TobaccoModule {
+}
