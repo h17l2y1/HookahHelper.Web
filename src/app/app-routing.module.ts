f5b8fe46f14a65ca8fe1efcdaba48ae1e812a885
vlad <vladislavvillya@gmail.com>
2023-07-23 01:33:32 +0300

    fix empty url after redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   { path: 'tobacco/:id', loadChildren: () => import('./tobacco/tobacco.module').then(m => m.TobaccoModule) },
+  { path: 'tobacco', loadChildren: () => import('./tobacco/tobacco.module').then(m => m.TobaccoModule) },
   { path: 'brand', loadChildren: () => import('./brand/brand.module').then(m => m.BrandModule) },
+  {path: '', redirectTo: 'brand', pathMatch: 'full'},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
