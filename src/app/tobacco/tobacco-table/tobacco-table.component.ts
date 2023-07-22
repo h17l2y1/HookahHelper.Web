a8df3fe3f76d7e964555f1d77488f94a9826abdb
Artem <artem9121997@gmail.com>
2023-07-22 19:01:46 +0300

    add tobacco paginator

--- src/app/tobacco/tobacco-table/tobacco-table.component.ts
+++ src/app/tobacco/tobacco-table/tobacco-table.component.ts
@@ -2,12 +2,7 @@ import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {TobaccoCreateComponent} from "../tobacco-create/tobacco-create.component";
 import {TobaccoService} from "../tobacco.service";
-import {map, merge, startWith, switchMap} from "rxjs";
-import {MatTableDataSource} from "@angular/material/table";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatSort} from "@angular/material/sort";
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
 import {Tobacco} from "../../interfaces/entity/tobacco";
 import {GetAllResponse} from "../../interfaces/models/get-all-response";
 
@@ -17,20 +12,18 @@ import {GetAllResponse} from "../../interfaces/models/get-all-response";
   styleUrls: ['./tobacco-table.component.scss']
 })
 export class TobaccoTableComponent implements AfterViewInit  {
-  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   public readonly displayedColumns: string[] = ['image', 'name', 'description', 'country'];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   public totalRows = 0;
   public currentPage = 0;
-  public pageSize = 5;
-  public pageSizeOptions = [5, 10, 25, 100];
+  public pageSize = 2;
+  public pageSizeOptions = [2, 5, 10, 25, 100];
   public filterBy?: string;
   public isLoadingResults = true;
 
   public tobaccos!: Tobacco[];
-
 
   constructor(
     public dialog: MatDialog,
@@ -59,31 +52,17 @@ export class TobaccoTableComponent implements AfterViewInit  {
     });
   }
 
-  // private getTobaccos(): void {
-  //   merge(this.sort.sortChange, this.paginator.page)
-  //     .pipe(
-  //       startWith({}),
-  //       switchMap(() => {
-  //         this.isLoadingResults = true;
-  //         return this.tobaccoService.getAll(this.paginator.pageIndex, this.pageSize, this.sort.direction, this.sort.active, this.filterBy)
-  //       }),
-  //       map(data => {
-  //         this.isLoadingResults = false;
-  //         this.totalRows = data.total;
-  //         return data.list;
-  //       }),
-  //     )
-  //     .subscribe((data: Tobacco[]) => {
-  //       this.dataSource = new MatTableDataSource<Tobacco>(data);
-        // this.dataSource.data = data
-      // });
-  // }
-
   public getTobaccos(): void {
     this.tobaccoService.getAll(this.paginator.pageIndex, this.pageSize, 'asc', 'name', this.filterBy)
       .subscribe((data: GetAllResponse<Tobacco>) => {
-        // this.dataSource = new MatTableDataSource<Tobacco>(data.list);
         this.tobaccos = data.list;
+        this.totalRows =data.total
       });
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.pageSize = e.pageSize;
+    this.currentPage = e.pageIndex;
+    this.getTobaccos();
   }
 }
