38efa31155468eb48f0041d2a3302d0981fc1227
vlad <vladislavvillya@gmail.com>
2023-07-26 19:33:11 +0300

    added LinseService

---
+++ src/app/services/line.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Line} from "../interfaces/entity/line";
+
+@Injectable()
+export class LineService {
+
+  readonly rootUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getLinesByBrandId(id: string): Observable<Line[]> {
+    return this.http.get<Line[]>(this.rootUrl + `Line/GetLinesByBrandId/${id}`);
+  }
+
+  public getOptions(): Observable<Line[]> {
+    return this.http.get<Line[]>(this.rootUrl + `Line/GetOptions`);
+  }
+}
