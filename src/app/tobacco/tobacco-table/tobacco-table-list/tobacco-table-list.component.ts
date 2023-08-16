8b087d1114aa79a5b4b057b0447ea43b21bc9c37
Artem <artem9121997@gmail.com>
2023-08-16 18:50:39 +0300

    add delete to tobacco-table-list

--- src/app/tobacco/tobacco-table/tobacco-table-list/tobacco-table-list.component.ts
+++ src/app/tobacco/tobacco-table/tobacco-table-list/tobacco-table-list.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Tobacco} from "../../../interfaces/entity/tobacco";
 import {tap} from "rxjs";
 import {TobaccoEditorComponent} from "../../tobacco-editor/tobacco-editor.component";
 import {ENTER_ANIMATION_DURATION, EXIT_ANIMATION_DURATION} from "../../../constants";
 import {MatDialog} from "@angular/material/dialog";
 import {TobaccoService} from "../../tobacco.service";
+import {ConfirmationPopupComponent} from "../../../shared/components/confirmation-popup/confirmation-popup.component";
 
 @Component({
   selector: 'app-tobacco-table-list',
@@ -13,13 +14,15 @@ import {TobaccoService} from "../../tobacco.service";
 })
 export class TobaccoTableListComponent {
   @Input() tobaccos!: Tobacco[];
+  @Output("getTobaccosEmit") getTobaccos: EventEmitter<any> = new EventEmitter();
   public isLoadingResults = true;
   public readonly displayedColumns: string[] = ['image', 'name', 'description', 'action'];
 
   constructor(
     public dialog: MatDialog,
     private tobaccoService: TobaccoService
-  ) {}
+  ) {
+  }
 
   public onEdit(id: string): void {
     this.tobaccoService.getById(id).pipe(
@@ -43,6 +46,19 @@ export class TobaccoTableListComponent {
         });
       }))
       .subscribe();
+  }
+
+  public onDelete(id: string): void {
+    const dialogRef = this.dialog.open(ConfirmationPopupComponent, {
+      width: "300px"
+    });
+    dialogRef.afterClosed().subscribe(popupResponse => {
+      if (popupResponse) {
+        this.tobaccoService.remove(id).pipe(
+          tap(() => this.getTobaccos.emit())
+        ).subscribe();
+      }
+    });
   }
 
 }
